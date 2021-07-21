80d87b2fb48304d75e26b36fab872aa4fdbc2b9b
Ederson Torresini <boidacarapreta@gmail.com>
2021-07-21 11:09:27 -0300

    Criar comandos de rotação de jogador

--- plugins/labirinto/labirinto.py
+++ plugins/labirinto/labirinto.py
@@ -34,7 +34,7 @@ class Labirinto(BotPlugin):
         Converter número inteiro em string de 32 bits.
         """
 
-        return f'{inteiro:032b}'
+        return f"{inteiro:032b}"
 
     def posicao_do_jogador(self):
         """
@@ -60,16 +60,86 @@ class Labirinto(BotPlugin):
                 Como o Python usa limite fechado a esquerda e aberto a direita,
                 o intervalo vai de 27 (inclui) a 31 (não inclui).
                 """
-                if sentido == '0001':
+                if sentido == "0001":
                     return x, y, "N"
-                elif sentido == '0010':
+                elif sentido == "0010":
                     return x, y, "S"
-                elif sentido == '0100':
+                elif sentido == "0100":
                     return x, y, "O"
-                elif sentido == '1000':
+                elif sentido == "1000":
                     return x, y, "L"
                 y += 1
             x += 1
+
+    def atualizar_posicao_do_jogador(self, rotação):
+        """
+        Atualizar a posição ou sentido do jogador no mapa,
+        que por se tratar de uma matriz de inteiros é feita
+        uma operação de soma/subtração nas células para
+        atualizar os dados do jogador.
+        """
+
+        x, y, sentido = self.posicao_do_jogador()
+        if rotação == "direita":
+            if sentido == "N":
+                """
+                Norte -> Leste
+                - 2 (N) + 16 (L)
+                """
+                rotacionar = +14
+            elif sentido == "S":
+                """
+                Sul -> Oeste
+                - 4 (S) + 8 (O)
+                """
+                rotacionar = +4
+            elif sentido == "O":
+                """
+                Oeste -> Norte
+                - 8 (O) + 2 (N)
+                """
+                rotacionar = -6
+            else:
+                """
+                Último caso é Leste ("L")
+                Leste -> Sul
+                 - 16 (L) + 4 (S)
+                """
+                rotacionar = -12
+        else:
+            """
+            Se a rotação não é para direita, então é esquerda
+            """
+            if sentido == "N":
+                """
+                Norte -> Oeste
+                - 2 (N) + 8 (O)
+                """
+                rotacionar = +6
+            elif sentido == "S":
+                """
+                Sul -> Leste
+                - 4 (S) + 16 (L)
+                """
+                rotacionar = +12
+            elif sentido == "O":
+                """
+                Oeste -> Sul
+                - 8 (O) + 4 (S)
+                """
+                rotacionar = -4
+            else:
+                """
+                Último caso é Leste ("L")
+                Leste -> Norte
+                - 16 (L) + 2 (N)
+                """
+                rotacionar = -14
+        """
+        Atualiza o mapa de inteiros e informa o usuário o novo sentido
+        """
+        self.mapa_inteiros[x][y] += rotacionar
+        return self.posicao_do_jogador()
 
     @re_botcmd(pattern=r"^(.*)mapa(.*)$")
     def mapa(self, msg, match):
@@ -85,11 +155,25 @@ class Labirinto(BotPlugin):
         """
         Informar a sentido do jogador como ponto cardeal.
         """
-        
+
         linha, coluna, sentido = self.posicao_do_jogador()
         yield "Posição no mapa: [" + str(linha) + "," + str(coluna) + "]"
         yield "Sentido (ponto cardeal): " + sentido
 
-    @re_botcmd(pattern=r"^(vir|gir)(a|e|ar)(.*)(direita|esquerda)(.*)$", matchall=True)
-    def vira(self, msg, match):
-        return "Virando para o lado, " + msg.frm.person
+    @re_botcmd(pattern=r"^(.*)direita(.*)$")
+    def direita(self, msg, match):
+        """
+        Rotacionar 90 graus o jogador para a direita - na sua perspectiva.
+        """
+
+        x, y, sentido = self.atualizar_posicao_do_jogador("direita")
+        yield "Novo sentido: " + sentido
+
+    @re_botcmd(pattern=r"^(.*)esquerda(.*)$")
+    def esquerda(self, msg, match):
+        """
+        Rotacionar 90 graus o jogador para a esquerda - na sua perspectiva.
+        """
+
+        x, y, sentido = self.atualizar_posicao_do_jogador("esquerda")
+        yield "Novo sentido: " + sentido
