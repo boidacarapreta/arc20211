c0809d78e32e2c1f431b60daf58f138c672b3021
Ederson Torresini <boidacarapreta@gmail.com>
2021-07-22 09:26:33 -0300

    Migrar código de dev para main
    Código revisado manualmente ao invés de merge por causa das aulas

--- config.py
+++ config.py
@@ -3,18 +3,41 @@ from os import environ
 from dotenv import load_dotenv
 load_dotenv()
 
+"""
+Backend: Discord
+"""
+BACKEND = "Discord"
 
-BACKEND = 'Discord'
+"""Necessário o arquivo .env no formato:
+DISCORD_TOKEN=<token>
+onde <token> contém o valor obtido de
+https://discord.com/developers/applications
+"""
 BOT_IDENTITY = {
- 'token': environ.get('DISCORD_TOKEN')
+    "token": environ.get("DISCORD_TOKEN")
 }
-BOT_ADMINS = (environ.get('BOT_ADMIN'), )
-BOT_PREFIX = '!'
+BOT_ADMINS = (environ.get("BOT_ADMIN"), )
+
+"""
+Prefixo do bot
+"""
+BOT_PREFIX = "!"
 BOT_PREFIX_OPTIONAL_ON_CHAT = True
 
-BOT_DATA_DIR = r'data'
-BOT_EXTRA_PLUGIN_DIR = r'plugins'
-BOT_EXTRA_BACKEND_DIR= r'backend'
+"""
+Diretórios do Errbot
+"""
+BOT_DATA_DIR = r"data"
+BOT_EXTRA_PLUGIN_DIR = r"plugins"
+BOT_EXTRA_BACKEND_DIR = r"backend"
 
-BOT_LOG_FILE = r'errbot.log'
+"""
+Se os plugins têm dependências, usar ambiente virtual e requirements.txt
+"""
+AUTOINSTALL_DEPS = True
+
+"""
+Log
+"""
+BOT_LOG_FILE = r"errbot.log"
 BOT_LOG_LEVEL = logging.DEBUG
--- plugins/labirinto/labirinto.py
+++ plugins/labirinto/labirinto.py
@@ -1,4 +1,5 @@
-from errbot import BotPlugin, botcmd
+from errbot import BotPlugin, re_botcmd
+from random import randint
 
 
 class Labirinto(BotPlugin):
@@ -18,20 +19,161 @@ class Labirinto(BotPlugin):
       - 16: sentido Leste
     Assim, o mapa acumula informações com base nessas
     potências de dois, como por exemplo:
-    5 = 4 + 1 = jogador no sentido Sul + sala ou corredor    
+    5 = 4 + 1 = jogador no sentido Sul + sala ou corredor
     """
 
-    mapa_inteiros = [[0, 0, 0, 17, 0],
-                     [0, 0, 0,  1, 0],
-                     [0, 0, 1,  1, 0],
-                     [0, 0, 1,  1, 1],
-                     [0, 0, 0,  0, 0]]
+    mapa_inteiros = [[0, 0, 0, 5, 0],
+                     [0, 0, 0, 1, 0],
+                     [0, 0, 1, 1, 0],
+                     [0, 0, 1, 1, 1],
+                     [0, 0, 0, 0, 0]]
 
-    @botcmd
-    def mapa(self, msg, args):
+    # https://stackoverflow.com/a/10411108/5167118
+    def converter_inteiro_para_binario(self, inteiro):
         """
-        Apresentar o mapa do bot.
+        Converter número inteiro em string de 32 bits.
         """
-        
+
+        return f"{inteiro:032b}"
+
+    def posicao_do_jogador(self):
+        """
+        Informar a orientação do jogador em relação ao mapa:
+        - Norte (N)
+        - Sul (S)
+        - Oeste (O)
+        - Leste (L)
+        """
+
+        x = 0
+        for linha in self.mapa_inteiros:
+            y = 0
+            for coluna in linha:
+                sentido = self.converter_inteiro_para_binario(coluna)[27:31]
+                """
+                Os sentidos estão organizados por bit,
+                a contar da direita para a esquerda:
+                - N: bit 30 (2^1)
+                - S: bit 29 (2^2)
+                - O: bit 28 (2^3)
+                - L: bit 27 (2^4)
+                Como o Python usa limite fechado a esquerda e aberto a direita,
+                o intervalo vai de 27 (inclui) a 31 (não inclui).
+                """
+                if sentido == "0001":
+                    return x, y, "N"
+                elif sentido == "0010":
+                    return x, y, "S"
+                elif sentido == "0100":
+                    return x, y, "O"
+                elif sentido == "1000":
+                    return x, y, "L"
+                y += 1
+            x += 1
+
+    def atualizar_posicao_do_jogador(self, rotação):
+        """
+        Atualizar a posição ou sentido do jogador no mapa,
+        que por se tratar de uma matriz de inteiros é feita
+        uma operação de soma/subtração nas células para
+        atualizar os dados do jogador.
+        """
+
+        x, y, sentido = self.posicao_do_jogador()
+        if rotação == "direita":
+            if sentido == "N":
+                """
+                Norte -> Leste
+                - 2 (N) + 16 (L)
+                """
+                rotacionar = +14
+            elif sentido == "S":
+                """
+                Sul -> Oeste
+                - 4 (S) + 8 (O)
+                """
+                rotacionar = +4
+            elif sentido == "O":
+                """
+                Oeste -> Norte
+                - 8 (O) + 2 (N)
+                """
+                rotacionar = -6
+            else:
+                """
+                Último caso é Leste ("L")
+                Leste -> Sul
+                 - 16 (L) + 4 (S)
+                """
+                rotacionar = -12
+        else:
+            """
+            Se a rotação não é para direita, então é esquerda
+            """
+            if sentido == "N":
+                """
+                Norte -> Oeste
+                - 2 (N) + 8 (O)
+                """
+                rotacionar = +6
+            elif sentido == "S":
+                """
+                Sul -> Leste
+                - 4 (S) + 16 (L)
+                """
+                rotacionar = +12
+            elif sentido == "O":
+                """
+                Oeste -> Sul
+                - 8 (O) + 4 (S)
+                """
+                rotacionar = -4
+            else:
+                """
+                Último caso é Leste ("L")
+                Leste -> Norte
+                - 16 (L) + 2 (N)
+                """
+                rotacionar = -14
+        """
+        Atualiza o mapa de inteiros e informa o usuário o novo sentido
+        """
+        self.mapa_inteiros[x][y] += rotacionar
+        return self.posicao_do_jogador()
+
+    @re_botcmd(pattern=r"^(.*)mapa(.*)$")
+    def mapa(self, msg, match):
+        """
+        Apresentar o mapa no bot.
+        """
+
         for linha in self.mapa_inteiros:
             yield " ".join(map(str, linha))
+
+    @re_botcmd(pattern=r"^(.*)(eu|sentido)(.*)$")
+    def jogador(self, msg, match):
+        """
+        Informar a sentido do jogador como ponto cardeal.
+        """
+
+        linha, coluna, sentido = self.posicao_do_jogador()
+        yield "Posição no mapa: [" + str(linha) + "," + str(coluna) + "]"
+        yield "Sentido (ponto cardeal): " + sentido
+
+    @re_botcmd(pattern=r"^(.*)direita(.*)$")
+    def direita(self, msg, match):
+        """
+        Rotacionar 90 graus o jogador para a direita - na sua perspectiva.
+        """
+
+        x, y, sentido = self.atualizar_posicao_do_jogador("direita")
+        yield "Novo sentido: " + sentido
+
+    @re_botcmd(pattern=r"^(.*)esquerda(.*)$")
+    def esquerda(self, msg, match):
+        """
+        Rotacionar 90 graus o jogador para a esquerda - na sua perspectiva.
+        """
+
+        x, y, sentido = self.atualizar_posicao_do_jogador("esquerda")
+        yield "Novo sentido: " + sentido
